3391f5d2ff1d415b444a15939fdd1ce180adccaf
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-05-29 14:52:00 -0300

    feat: home loading style created

---
+++ src/components/HomeLoading/style.js
@@ -0,0 +1,12 @@
+import { Backdrop, styled } from '@mui/material'
+
+export const CustomBackdrop = styled(Backdrop)`
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  width: 100%;
+  z-index: 1;
+`
