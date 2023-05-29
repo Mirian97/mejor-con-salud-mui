aadef34de756f35de24e6684909fcea0009e6cca
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-05-29 14:50:05 -0300

    style: file extension change to jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 import { ThemeProvider } from '@mui/material'
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import HomeLoading from './components/HomeLoading'
 import { GlobalProvider } from './contexts/GlobalContext'
 import MainRoutes from './routes'
 import './styles/index.css'
@@ -16,7 +17,9 @@ root.render(
       <GlobalProvider>
         <ThemeProvider theme={theme}>
           <ToastContainer />
-          <MainRoutes />
+          <Suspense fallback={<HomeLoading />}>
+            <MainRoutes />
+          </Suspense>
         </ThemeProvider>
       </GlobalProvider>
     </React.StrictMode>
