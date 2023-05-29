9e2242f20f600dbc1500ffcc79386f5a116902ed
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-05-29 14:27:21 -0300

    style: reorganization of routes and providers

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
+import { ThemeProvider } from '@mui/material'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { GlobalProvider } from './contexts/GlobalContext'
 import MainRoutes from './routes'
 import './styles/index.css'
+import theme from './theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <ToastContainer />
-      <MainRoutes />
+      <GlobalProvider>
+        <ThemeProvider theme={theme}>
+          <ToastContainer />
+          <MainRoutes />
+        </ThemeProvider>
+      </GlobalProvider>
     </React.StrictMode>
   </BrowserRouter>
 )
